3359390f0ec1fad42788c0fe5e2ab389a2f2b00b
nojiwon1227 <nojiwon1227@gmail.com>
2024-10-13 05:06:12 +0900

    @RequestBody 와 @ModelAttribute 동작 원리 공부

--- src/main/java/com/leeni/sample/controller/ApiController.java
+++ src/main/java/com/leeni/sample/controller/ApiController.java
@@ -4,7 +4,6 @@ import com.leeni.sample.dto.ResponseDTO;
 import com.leeni.sample.dto.user.RequestUserDTO;
 import com.leeni.sample.dto.user.ResponseUserDTO;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 
     @PostMapping("/login")
-    public ResponseDTO login(@ModelAttribute RequestUserDTO requestUserDTO) {
+    public ResponseDTO login(@RequestBody RequestUserDTO requestUserDTO) {
 
         ResponseUserDTO responseUserDTO = ResponseUserDTO.builder().build();
         log.info("responseUserDTO = {}", responseUserDTO);
--- src/main/java/com/leeni/sample/dto/user/RequestUserDTO.java
+++ src/main/java/com/leeni/sample/dto/user/RequestUserDTO.java
@@ -7,6 +7,12 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @ToString(callSuper = true)
 @NoArgsConstructor(access = AccessLevel.PRIVATE) // @SuperBuilder로 인해 자동으로 기본생성자가 만들어지지 않아 추가
+// 1. @RequestBody는 일반적인 상황에서는 기본 생성자가 꼭 필요하다.
+//-> RestController에서 @RequestBody를 바인딩을 하기 위해 ObjectMapper를 사용하는데 기본 생성자로 DTO를 생성하기 때문.
+//2. Property 기반 클래스(@JsonProperty, @JsonAutoDetect)인 경우 기본생성자 없이 생성 가능하다.
+//3. 생성자가 위임된 경우라면 기본생성자 없이 생성 가능하다.
+//4. 기본생성자가 없고, 인자를 받는 생성자가(인자 2개 이상) 있다면 생성 가능하다.
+//5. 기본생성자가 없고, 인자를 받는 생성자가 1개의 인자를 받는다면 @JsonCreator를 직접 추가해야 생성 가능하다.
 @Getter // @RequestBody는 기본생성자로 인스턴스(객체)를 생성하고 setter 또는 getter를 통해 feild 정보를 추출하여 값을 넣어줌
 // @ModelAttribute는 파라미터 개수가 0개인 기본 생성자가 확인되면 우선 인스턴스(객체)를 생성하고, setter 메서드를 통한 바인딩을 시도한다.
 // 그렇지 않을 경우 필드에 맞는 파라미터를 가진 생성자를 찾아 바인딩을 시도한다.
